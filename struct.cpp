2c76e8d481d9afeb9b15347ca5e9e5d8101be927
kzabc <qq@kzabc.com>
2019-02-18 22:33:35 +0800

    struct 增加修饰符

--- struct.cpp
+++ struct.cpp
@@ -4,19 +4,30 @@
 #include <iostream>
 using namespace std;
 struct test{
+
+private:
+        double x,y;
+
+public:
+    test(){};
+    test(double a, double b){
+        x=a;
+        y=b;
+    }
     void set(double a, double b){
 
         x=a;
         y=b;
     }
     void get(){
-        cout <<"x="<<x<<"y="<<y;
+        cout <<"x="<<x<<"y="<<y<<endl;
     }
-    double x,y;
+
 };
 int main(){
     double a(0),b(0);
-    test user;
+    test user(123.1,456.2);
+    user.get();
     cin >>a;
     cin >>b;
     user.set(a,b);
